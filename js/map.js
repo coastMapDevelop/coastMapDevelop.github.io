3482c887528b7d45431c2b7ff7e3060110ee07ec
schuylerwilliams <skywilliams@gmail.com>
2016-12-02 12:31:43 -0600

    updates

--- js/map.js
+++ js/map.js
@@ -1,7 +1,5 @@
 
 /*
-var simple = {
-	"version": 8,
 	"constants": {
 		"@name": "{name_en}",
 		"@sans": "Open Sans Regular, Arial Unicode MS Regular",
@@ -41,89 +39,11 @@ var simple = {
 			"base": 0.5,
 			"stops": [[4, 0.5], [10, 1.5], [20, 5]]
 		}
-	},
-	"sources": {
-		"osm": {
-			"type": "vector",
-			"tiles": ["https://vector.mapzen.com/osm/all/{z}/{x}/{y}.mvt?api_key=vector-tiles-LM25tq4"]
-		}
-	},
-	"layers": [
-		{
-			"id": "background",
-			"type": "background",
-			"paint": {
-				"background-color": "@land"
-			}
-		}, {
-			"id": "water-polygon",
-			"source": "osm",
-			"source-layer": "water",
-			"type": "fill",
-			"filter": ["==", "$type", "Polygon"],
-			"paint": {
-				"fill-color": "@water"
-			}
-		}, {
-			"id": "water-line",
-			"source": "osm",
-			"source-layer": "water",
-			"type": "line",
-			"filter": ["==", "$type", "LineString"],
-			"paint": {
-				"line-color": "@water",
-				"line-width": {
-					"base": 1.2,
-					"stops": [[8, 0.5], [20, 15]]
-				}
-			}
-		}
-	]
-};
+	}
 */
 
 var simple = {
     "version": 8,
-	"constants": {
-		"@name": "{name_en}",
-		"@sans": "Open Sans Regular, Arial Unicode MS Regular",
-		"@sans-it": "Open Sans Italic, Arial Unicode MS Regular",
-		"@sans-md": "Open Sans Semibold, Arial Unicode MS Bold",
-		"@sans-bd": "Open Sans Bold, Arial Unicode MS Bold",
-		"@big-label": "#cb4b49",
-		"@medium-label": "#f27a87",
-		"@small-label": "#384646",
-		"@label-halo": "rgba(255,255,255,0.5)",
-		"@label-halo-dark": "rgba(0,0,0,0.2)",
-		"@land": "#ededed",
-		"@water": "#7acad0",
-		"@park": "#c2cd44",
-		"@building": "#afd3d3",
-		"@highway": "#5d6765",
-		"@road": "#c0c4c2",
-		"@path": "#5d6765",
-		"@subway": "#ef7369",
-		"@highway-width": {
-			"base": 1.55,
-			"stops": [[4, 0.5], [8, 1.5], [20, 40]]
-		},
-		"@road-width": {
-			"base": 1.55,
-			"stops": [[4, 0.25], [20, 30]]
-		},
-		"@path-width": {
-			"base": 1.8,
-			"stops": [[10, 0.15], [20, 15]]
-		},
-		"@road-misc-width": {
-			"base": 1,
-			"stops": [[4, 0.25], [20, 30]]
-		},
-		"@stream-width":{
-			"base": 0.5,
-			"stops": [[4, 0.5], [10, 1.5], [20, 5]]
-		}
-	},
     "sources": {
         "osm": {
             "type": "vector",
@@ -135,7 +55,7 @@ var simple = {
             "id": "background",
             "type": "background",
             "paint": {
-                "background-color": "@land"
+                "background-color": "#ededed"
             }
         }, {
             "id": "water",
@@ -144,7 +64,7 @@ var simple = {
             "source-layer": "water",
             "filter": ["==", "$type", "Polygon"],
             "paint": {
-                "fill-color": "@water"
+                "fill-color": "#7acad0"
             }
         }
     ]
