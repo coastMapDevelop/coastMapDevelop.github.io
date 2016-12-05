5db9ff28fd26048cf4e2b2908f016c87fba56af1
schuylerwilliams <skywilliams@gmail.com>
2016-12-05 17:55:49 -0600

    add fusion table

--- testGoogleAPI/js/map.js
+++ testGoogleAPI/js/map.js
@@ -7,4 +7,11 @@ function initMap() {
 		center: {lat: 43.0731, lng: -89.4012},
 		zoom: 6
 	});
+	
+	var layer = new google.maps.FusionTablesLayer({
+		query: {
+			select: 'geometry',
+			from: '1l2ADyWAVukfgStGrFCXa9F3TrJIy-jB3V8nV5a4g'
+		}
+	});
 };
