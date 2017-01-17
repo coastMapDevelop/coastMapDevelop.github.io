2d57c56804eed1f6b3ff489f2ce240bea863c6f1
schuylerwilliams <skywilliams@gmail.com>
2017-01-17 12:39:52 -0600

    update

--- testGoogleSpreadsheet/js/initMapLeaf.js
+++ testGoogleSpreadsheet/js/initMapLeaf.js
@@ -250,8 +250,10 @@ function main() {
 					var m;
 					for (m=0; m < popupCountyArr.length; m++) {
 						var link = googleSpreadsheet[i][popupCountyArr[m][2]];
+						console.log(link);
 						if (link == null) {
 							// deactivate link
+							document.getElementById(popupCountyArr[m][0]).setAttribute("href", "");
 						} else {
 							// activate link
 							document.getElementById(popupCountyArr[m][0]).setAttribute("href", link);
