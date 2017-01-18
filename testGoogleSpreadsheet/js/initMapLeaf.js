1dae0a4cd0c2974a2e5b7fd825e4b4aae38fe428
schuylerwilliams <skywilliams@gmail.com>
2017-01-18 16:43:16 -0600

    try deleting if no link

--- testGoogleSpreadsheet/js/initMapLeaf.js
+++ testGoogleSpreadsheet/js/initMapLeaf.js
@@ -261,8 +261,8 @@ function main() {
 						var link = googleSpreadsheet[i][popupCountyArr[m][2]];
 						if (link == 'null') {
 							// deactivate link
-							// use buttons to deactivate, instead of anchor tags
-							document.getElementById(popupCountyArr[m][0]).style.visibility = "hidden";
+							//document.getElementById(popupCountyArr[m][0]).style.visibility = "hidden";
+							$(popupCountyArr[m][0]).remove();
 						} else {
 							// activate link
 							document.getElementById(popupCountyArr[m][0]).setAttribute("href", link);
