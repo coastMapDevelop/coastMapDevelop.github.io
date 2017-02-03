4b4e1df95ccadbb58d08b5e3bab447f257edab20
Schuyler Williams <skywilliams@gmail.com>
2017-02-03 12:44:53 -0600

    comment some css

--- testGoogleSpreadsheet/js/index.js
+++ testGoogleSpreadsheet/js/index.js
@@ -52,65 +52,6 @@ function listMajors() {
 	});
 };
 
-/*
-function expMenuButton() {
-	var menu = document.getElementById("expMenu");
-	var active = menu.classList.contains('active');
-	
-	var layer1 = document.getElementById("expLayer01");
-		layer2 = document.getElementById("expLayer02");
-		layer3 = document.getElementById("expLayer03");
-		layer4 = document.getElementById("expLayer04");
-	
-	var icon = document.getElementById("menuIcon1");
-	if (active == false) {
-		menu.classList.add('active');
-		
-		icon.classList.remove('fa', 'fa-bars', 'fa-2x');
-		icon.classList.add('fa', 'fa-times', 'fa-2x');
-		
-		layer1.style.opacity = "1";
-		layer1.style.visibility = "visible";
-		layer1.style.top = "-43px";
-	
-		layer2.style.opacity = "1";
-		layer2.style.visibility = "visible";
-		layer2.style.right = "60px";
-	
-		layer3.style.opacity = "1";
-		layer3.style.visibility = "visible";
-		layer3.style.top = "50px";
-	
-		layer4.style.opacity = "1";
-		layer4.style.visibility = "visible";
-		layer4.style.right = "-75px";
-	} else if (active == true) {
-		menu.classList.remove('active');
-		
-		icon.classList.remove('fa', 'fa-times', 'fa-2x');
-		icon.classList.add('fa', 'fa-bars', 'fa-2x');
-		
-		layer1.style.opacity = "";
-		layer1.style.top = "";
-	
-		layer2.style.opacity = "";
-		layer2.style.right = "";
-	
-		layer3.style.opacity = "";
-		layer3.style.top = "";
-	
-		layer4.style.opacity = "";
-		layer4.style.right = "";
-		
-		setTimeout(function(){ 
-			layer1.style.visibility = "";
-			layer2.style.visibility = "";
-			layer3.style.visibility = "";
-			layer4.style.visibility = "";
-		}, 500);
-	}
-};
-*/
 
 // function to display base map container on click
 function baseClick() {
@@ -285,15 +226,6 @@ function uiHover(source, num) {
 
 
 
-/*
-var baseMapArray = [
-	"tile01",
-	"tile02",
-	"tile03",
-	"tile04",
-	"tile05"
-];
-*/
 
 
 var supSideArr = [
