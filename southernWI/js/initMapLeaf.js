0ba5360e16533061dcb1ee97543d2e43a89957f3
schuylerwilliams <skywilliams@gmail.com>
2017-03-08 14:38:11 -0600

    update

--- southernWI/js/initMapLeaf.js
+++ southernWI/js/initMapLeaf.js
@@ -595,12 +595,13 @@ function main() {
 			var center = layer.getBounds().getCenter();
 			console.log(center);
 			var wantToChange = center.lng + .06;
+			console.log(wantToChange);
 			
 			removeMarkers();
 			countyClickedZoomControl = true;
 			clickedCountyName.push(layer.feature.properties.NAME10);
 			
-			map.setView(wantToChange, 10);
+			map.setView(center, 10);
 			crossReference(e, layer, layer.feature.properties, layer.feature.geometry.type, layer.options.fillColor, "click"); // call function to cross reference clicked layer name with google spreadsheet data
 
 			window.setTimeout(function() {
